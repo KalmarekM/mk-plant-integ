eb8f2d8bbd54851be2d05dc3c4cbfb8e87a0f152
Marek <kalmarek@poczta.onet.pl>
2026-03-10 19:24:58 +0100

    fix sensor (v1.0.10)

--- custom_components/plant_mk/config_flow.py
+++ custom_components/plant_mk/config_flow.py
@@ -1,86 +1,72 @@
 import voluptuous as vol # type: ignore
 from homeassistant import config_entries # type: ignore
-from homeassistant.core import callback  # type: ignore
+from homeassistant.core import callback # type: ignore
 from homeassistant.helpers.selector import ( # type: ignore
-    EntitySelector,
-    EntitySelectorConfig,
-    NumberSelector,
-    NumberSelectorConfig,
-    NumberSelectorMode,
+    EntitySelector, EntitySelectorConfig,
+    NumberSelector, NumberSelectorConfig, NumberSelectorMode,
 )
 from .const import DOMAIN
 
+def plant_schema(defaults):
+    """Pomocnicza funkcja generująca spójny schemat dla instalacji i opcji."""
+    return vol.Schema({
+        vol.Required("min_moisture", default=defaults.get("min_moisture", 20)): NumberSelector(
+            NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
+        ),
+        vol.Required("max_moisture", default=defaults.get("max_moisture", 60)): NumberSelector(
+            NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
+        ),
+        vol.Required("min_temp", default=defaults.get("min_temp", 15)): NumberSelector(
+            NumberSelectorConfig(min=0, max=50, unit_of_measurement="°C", mode=NumberSelectorMode.BOX)
+        ),
+        vol.Required("max_temp", default=defaults.get("max_temp", 30)): NumberSelector(
+            NumberSelectorConfig(min=0, max=50, unit_of_measurement="°C", mode=NumberSelectorMode.BOX)
+        ),
+        vol.Required("min_humi", default=defaults.get("min_humi", 30)): NumberSelector(
+            NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
+        ),
+        vol.Required("max_humi", default=defaults.get("max_humi", 80)): NumberSelector(
+            NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
+        ),
+    })
+
 class MKPlantConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for MK Plant System."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input["plant_name"], data=user_input)
+            # Walidacja min < max
+            if user_input["min_moisture"] >= user_input["max_moisture"]:
+                errors["base"] = "min_moisture_error"
+            else:
+                return self.async_create_entry(title=user_input["plant_name"], data=user_input)
 
-        data_schema = vol.Schema({
+        # Do instalacji dodajemy jeszcze nazwę i sensory
+        full_schema = vol.Schema({
             vol.Required("plant_name"): str,
-            
-            # Wilgotność gleby
-            vol.Required("moisture_sensor"): EntitySelector(
-                EntitySelectorConfig(domain="sensor", device_class="moisture")
-            ),
-            vol.Required("min_moisture", default=20): NumberSelector(
-                NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
-            ),
-            vol.Required("max_moisture", default=60): NumberSelector(
-                NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
-            ),
+            vol.Required("moisture_sensor"): EntitySelector(EntitySelectorConfig(domain="sensor")),
+            vol.Required("temp_sensor"): EntitySelector(EntitySelectorConfig(domain="sensor")),
+            vol.Required("humi_sensor"): EntitySelector(EntitySelectorConfig(domain="sensor")),
+        }).extend(plant_schema({}).schema)
 
-            # Temperatura
-            vol.Required("temp_sensor"): EntitySelector(
-                EntitySelectorConfig(domain="sensor", device_class="temperature")
-            ),
-            vol.Required("min_temp", default=15): NumberSelector(
-                NumberSelectorConfig(min=0, max=50, unit_of_measurement="°C", mode=NumberSelectorMode.BOX)
-            ),
-            vol.Required("max_temp", default=30): NumberSelector(
-                NumberSelectorConfig(min=0, max=50, unit_of_measurement="°C", mode=NumberSelectorMode.BOX)
-            ),
-            
-            # Wilgotność powietrza
-            vol.Required("humi_sensor"): EntitySelector(
-                EntitySelectorConfig(domain="sensor", device_class="humidity")
-            ),
-            vol.Required("min_humi", default=30): NumberSelector(
-                NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
-            ),
-            vol.Required("max_humi", default=80): NumberSelector(
-                NumberSelectorConfig(min=0, max=100, mode=NumberSelectorMode.SLIDER)
-            ),
-        })
+        return self.async_show_form(step_id="user", data_schema=full_schema, errors=errors)
 
-        return self.async_show_form(step_id="user", data_schema=data_schema)
-    
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
         return MKPlantOptionsFlowHandler(config_entry)
-    
-class MKPlantOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options for the plant (editing)."""
 
+class MKPlantOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        """Manage the options."""
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            # Kluczowe: aktualizujemy dane w entry.data, a nie tylko w options
+            new_data = dict(self.config_entry.data)
+            new_data.update(user_input)
+            self.hass.config_entries.async_update_entry(self.config_entry, data=new_data)
+            return self.async_create_entry(title="", data={})
 
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema({
-                vol.Required("min_moisture", default=self.config_entry.data.get("min_moisture", 20)): int,
-                vol.Required("max_moisture", default=self.config_entry.data.get("max_moisture", 60)): int,
-                vol.Required("min_temp", default=self.config_entry.data.get("min_temp", 15)): int,
-                vol.Required("max_temp", default=self.config_entry.data.get("max_temp", 30)): int,
-                vol.Required("min_humi", default=self.config_entry.data.get("min_humi", 30)): int,
-                vol.Required("max_humi", default=self.config_entry.data.get("max_humi", 80)): int,
-            })
-        )
+        return self.async_show_form(step_id="init", data_schema=plant_schema(self.config_entry.data))
--- custom_components/plant_mk/sensor.py
+++ custom_components/plant_mk/sensor.py
@@ -8,23 +8,19 @@ from homeassistant.const import PERCENTAGE, UnitOfTemperature # type: ignore
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up sensors for a specific plant entry."""
+    # Pobieramy dane z entry.data (zawsze aktualne po poprawce w config_flow)
     config = entry.data
-    plant_name = config.get("plant_name", "Unknown Plant")
-    
+    plant_name = config.get("plant_name", "Plant")
+
     sensors = [
-        MKPlantNumericSensor(hass, plant_name, "moisture", config.get("moisture_sensor"), config.get("min_moisture", 20), config.get("max_moisture", 60)),
-        MKPlantNumericSensor(hass, plant_name, "temperature", config.get("temp_sensor"), config.get("min_temp", 15), config.get("max_temp", 30)),
-        MKPlantNumericSensor(hass, plant_name, "humidity", config.get("humi_sensor"), config.get("min_humi", 30), config.get("max_humi", 80)),
+        MKPlantNumericSensor(hass, plant_name, "moisture", config.get("moisture_sensor"), config.get("min_moisture"), config.get("max_moisture")),
+        MKPlantNumericSensor(hass, plant_name, "temperature", config.get("temp_sensor"), config.get("min_temp"), config.get("max_temp")),
+        MKPlantNumericSensor(hass, plant_name, "humidity", config.get("humi_sensor"), config.get("min_humi"), config.get("max_humi")),
     ]
-    
     async_add_entities(sensors)
 
 class MKPlantNumericSensor(SensorEntity):
-    """Numerical sensor for plant parameters."""
-
     def __init__(self, hass, plant_name, param_type, source_id, min_val, max_val):
-        """Initialize the sensor."""
         self.hass = hass
         self._plant_name = plant_name
         self._param_type = param_type
@@ -34,11 +30,14 @@ class MKPlantNumericSensor(SensorEntity):
         self._attr_unique_id = f"{plant_name}_{param_type}"
 
         if param_type == "temperature":
-            self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
             self._attr_device_class = SensorDeviceClass.TEMPERATURE
-        else:
+            self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+        elif param_type == "moisture":
+            self._attr_device_class = SensorDeviceClass.MOISTURE # Poprawione!
             self._attr_native_unit_of_measurement = PERCENTAGE
+        else:
             self._attr_device_class = SensorDeviceClass.HUMIDITY
+            self._attr_native_unit_of_measurement = PERCENTAGE
 
         self._attr_state_class = SensorStateClass.MEASUREMENT
 
